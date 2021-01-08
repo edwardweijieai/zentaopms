552221024f3efb922667be170e998dfa1d5a598c
edwardweijieai <edwardweijieai@gmail.com>
2021-01-08 05:46:27 +0000

    [fixed] install config

--- bin/deletelog.sh
+++ bin/deletelog.sh
@@ -1,1 +1,1 @@
-/usr/bin/php /workspace/zentaopms/bin/ztcli 'https://8001-e62d1cda-7616-4661-9f12-4a6a6ec7856b.ws-us03.gitpod.io/zentaopms/index.php?m=admin&f=deleteLog'
+/usr/bin/php /workspace/zentaopms/bin/ztcli 'https://8001-e62d1cda-7616-4661-9f12-4a6a6ec7856b.ws-us03.gitpod.io:8001/index.php?m=admin&f=deleteLog'
